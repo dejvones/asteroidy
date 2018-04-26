c5167cfd2b35adfa7771be21e0e39faa1d40fb44
David Vlasák <vla35123@spseol.cz>
2018-04-26 14:07:20 +0200

    init commit

---
+++ asteroidy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 26 13:26:15 2018
+
+@author: vla35123
+"""
+
+import pyglet
+window = pyglet.window.Window()
+pyglet.app.run()
+print('Hotovo!')
