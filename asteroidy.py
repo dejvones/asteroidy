735767437ad137d01fbcb064af6827850d022b5b
David Vlasák <vla35123@spseol.cz>
2018-05-10 14:07:02 +0200

    Sipky

--- asteroidy.py
+++ asteroidy.py
@@ -7,7 +7,8 @@ Created on Thu Apr 26 13:26:15 2018
 """
 
 import pyglet
-from pyglet.window.key import MOD_CTRL, A, B, C
+from math import sin, cos, pi
+from pyglet.window.key import DOWN, UP, LEFT, RIGHT
 
 window = pyglet.window.Window()
 
@@ -16,31 +17,69 @@ obrazek.anchor_x = obrazek.width //2
 obrazek.anchor_y = obrazek.height //2
 sprite = pyglet.sprite.Sprite(obrazek)
 
+uhel = 0
+sprite.rotation = 0
+rychlost = 20
+klavesy = set()
 
 def tiktak(t):
-    sprite.x = sprite.x + 10*t
-    sprite.y = sprite.y + 10*t
+    #posun
+    sprite.x = sprite.x + rychlost*t*sin((pi*uhel)/180)
+    sprite.y = sprite.y + rychlost*t*cos((pi*uhel)/180)
+    
+    #natočení
+    global rychlost
+    global uhel
+    for sym in klavesy:
+        if sym == DOWN:
+            if rychlost - 20 >= 0:
+                rychlost -= 20
+            else:
+                pass
+        elif sym == UP:
+            if rychlost + 20 <= 100:
+                rychlost += 20
+            else:
+                pass
+        elif sym == RIGHT:
+            sprite.rotation +=10
+            uhel += 10
+        elif sym == LEFT:
+            sprite.rotation -=10
+            uhel -=10
+        pass
+
 
 @window.event
 def on_draw():
     window.clear()
     sprite.draw()
-    
+
 
 @window.event
 def on_mouse_press(x, y, button, mod):
+    global uhel
     if button == 1:
         sprite.x = x
         sprite.y = y
     elif button == 4:
         sprite.rotation += 10
+        uhel += 10
 
 @window.event
 def on_key_press(sym, mod):
-    if sym != A:
-        print(sym, mod)
+    global klavesy
+    klavesy.add(sym)
+    
+    
+def on_key_release(sym, mod):
+    global klavesy
+    klavesy.remove(sym) 
+    
 
-pyglet.clock.schedule_interval(tiktak, 1/10)
+
+
+pyglet.clock.schedule_interval(tiktak, 1/60)
 
 
 pyglet.app.run()
